ea010511ae044037095dd421eb2b6973ce7294f0
Mark Serbol <markserbol@gmail.com>
2025-10-15 13:15:54 +0800

    Configure Docusaurus for GitHub Pages deployment
    - Update projectName to match repository name
    - Set onBrokenLinks to warn instead of throw
    - Add trailingSlash configuration
    - Update navbar and footer with ZipFood branding
    - Configure custom deploy script with SSH alias

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,13 +19,16 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs/',
+  
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ZipFood-IO', // Usually your GitHub org/user name.
-  projectName: 'zipfood-docs', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
