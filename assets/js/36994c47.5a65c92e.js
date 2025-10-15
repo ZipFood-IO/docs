25c8ea26baa56e3d0d79d6ce7066bf055dce125f
Mark Serbol <markserbol@gmail.com>
2025-10-15 13:10:34 +0800

    Deploy website - based on 25a88df8eb6f7b8460d28796f47b99d67eba982f

---
+++ assets/js/36994c47.5a65c92e.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkzipfood_docs=globalThis.webpackChunkzipfood_docs||[]).push([[9858],{5516:o=>{o.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ assets/js/acecf23e.c0256bb9.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkzipfood_docs=globalThis.webpackChunkzipfood_docs||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/docs/blog","blogTitle":"Blog","authorsListPath":"/docs/blog/authors"}')}}]);
